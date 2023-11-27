55e109fae37f54e6f6a8023cfec38c606a25d4fe
Philip Guldberg <philip180201@gmail.com>
2023-11-27 10:28:21 +0100

    added LoginView

--- src/mytunes/gui/controller/LoginViewController.java
+++ src/mytunes/gui/controller/LoginViewController.java
@@ -1,4 +1,11 @@
 package mytunes.gui.controller;
 
+import javafx.event.ActionEvent;
+
 public class LoginViewController {
+    public void HandleRememberMe(ActionEvent actionEvent) {
+    }
+
+    public void handleLogIn(ActionEvent actionEvent) {
+    }
 }
