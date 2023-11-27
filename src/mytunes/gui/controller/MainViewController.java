e58af30bed503d77840d84756d85221a839cc5f8
Philip Guldberg <philip180201@gmail.com>
2023-11-27 10:10:58 +0100

    mainview

--- src/mytunes/gui/controller/MainViewController.java
+++ src/mytunes/gui/controller/MainViewController.java
@@ -1,4 +1,15 @@
 package mytunes.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
 public class MainViewController {
+    public void HandleNewPlaylist(ActionEvent actionEvent) {
+    }
+
+    public void HandleMainMenu(ActionEvent actionEvent) {
+    }
+
+    public void handleSearchField(KeyEvent keyEvent) {
+    }
 }
