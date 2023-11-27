26536929a42eee7898a4fc5f7b180cee8a742d7c
Philip Guldberg <philip180201@gmail.com>
2023-11-27 13:03:09 +0100

    added popup window to MainViewController

--- src/mytunes/gui/controller/MainViewController.java
+++ src/mytunes/gui/controller/MainViewController.java
@@ -2,15 +2,25 @@ package mytunes.gui.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainViewController {
 
+    @FXML
+    private ButtonBar btnBarSong;
     @FXML
     private VBox vBoxDefault;
     @FXML
@@ -26,6 +36,9 @@ public class MainViewController {
     public void initialize() {
         // MAKES the TBT VIEW INVISIBLE
         tblViewSearch.setVisible(false);
+
+        // MAKES the BUTTON BAR INVISIBLE
+        btnBarSong.setVisible(false);
     }
 
 
@@ -36,6 +49,14 @@ public class MainViewController {
 
     @FXML
     private void HandleMainMenu(ActionEvent actionEvent) {
+        // RESETS the SEARCH FIELD
+        txtSearchField.setText(null);
+        // MAKES the TBT VIEW INVISIBLE
+        tblViewSearch.setVisible(false);
+        // MAKES the VBOX VISIBLE
+        vBoxDefault.setVisible(true);
+        // MAKES the BUTTON BAR INVISIBLE
+        btnBarSong.setVisible(false);
     }
 
     @FXML
@@ -44,24 +65,39 @@ public class MainViewController {
         if (keyEvent.getCode() == KeyCode.ENTER) {
             // Looks TO SEE if the TXT SEARCH FIELD is NOT EMPTY
             if (!(txtSearchField.getText().isEmpty())) {
+
                 // MAKES the TBL VIEW VISIBLE
                 tblViewSearch.setVisible(true);
+
                 // MAKES the VBOX INVISIBLE
                 vBoxDefault.setVisible(false);
+
+                // MAKES the BUTTON BAR VISIBLE
+                btnBarSong.setVisible(true);
             }
             // Looks TO SEE if the TXT SEARCH FIELD is EMPTY
             if (txtSearchField.getText().isEmpty()) {
+
                 // MAKES the TBT VIEW INVISIBLE
                 tblViewSearch.setVisible(false);
+
                 // MAKES the VBOX VISIBLE
                 vBoxDefault.setVisible(true);
+
+                // MAKES the BUTTON BAR INVISIBLE
+                btnBarSong.setVisible(false);
             }
         }
     }
 
     @FXML
-    private void handelCreate(ActionEvent actionEvent) {
-
+    private void handelCreate(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/CreateUpdateSongView.fxml"));
+        Parent popupWindow = loader.load();
+        Stage PopupWindow = new Stage();
+        PopupWindow.setScene(new Scene(popupWindow));
+        PopupWindow.initModality(Modality.APPLICATION_MODAL);
+        PopupWindow.showAndWait();
     }
 
     @FXML
