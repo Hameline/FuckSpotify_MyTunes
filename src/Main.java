1cbb9e01b80cf5ab35725189aa43b3454ef9cf87
Philip Guldberg <philip180201@gmail.com>
2023-11-27 11:50:10 +0100

    yalla

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/MovieView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/MainView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
--- src/mytunes/gui/controller/MainViewController.java
+++ src/mytunes/gui/controller/MainViewController.java
@@ -1,17 +1,49 @@
 package mytunes.gui.controller;
 
+import com.sun.jdi.connect.spi.TransportService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 
 public class MainViewController {
+
+    @FXML
+    private VBox vBoxDefault;
+    @FXML
+    private TableView tblViewSearch;
+    @FXML
+    private TableColumn tblViewPlaylistPlaylist;
+    @FXML
+    private TableColumn tblViewPlaylistUser;
+    @FXML
+    private TextField txtSearchField;
+
+
     @FXML
     private void HandleNewPlaylist(ActionEvent actionEvent) {
     }
+
     @FXML
     private void HandleMainMenu(ActionEvent actionEvent) {
     }
+
     @FXML
     private void handleSearchField(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            if (!(txtSearchField.getText().isEmpty())) {
+                tblViewSearch.isVisible();
+                vBoxDefault.isDisabled();
+            }
+
+            if (txtSearchField.getText().isEmpty()) {
+                tblViewSearch.isDisabled();
+                vBoxDefault.isVisible();
+            }
+        }
     }
 }
